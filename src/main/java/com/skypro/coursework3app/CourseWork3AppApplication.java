a85d1ea47fdb7eeeed68aabc62abae915d9f37b8
SIDORYCH <vladimiriv@hotmail.com>
2023-02-23 17:04:44 +0300

    Первый коммит

---
+++ src/main/java/com/skypro/coursework3app/CourseWork3AppApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.coursework3app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork3AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWork3AppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/coursework3app/CourseWork3AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.coursework3app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork3AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
